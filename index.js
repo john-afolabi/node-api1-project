89e8c536d1cb9c96688683d17fd8f4f62f83bafc
John Afolabi <johnafolabi.da@gmail.com>
2020-01-13 13:57:49 +0100

    Implement get users endpoint

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const express = require("express");
 const cors = require("cors");
 
 const { find, findById, insert, update, remove } = require("./data/db");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+  find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+app.listen(5000, () => {
+  console.log("API started on port 5000");
+});
