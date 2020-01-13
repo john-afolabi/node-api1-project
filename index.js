09e9028e2f1e1e2b2b5787a4d2d460a5b8861056
John Afolabi <johnafolabi.da@gmail.com>
2020-01-13 14:06:45 +0100

    Implement get user by id endpoint

--- index.js
+++ index.js
@@ -21,6 +21,25 @@ app.get("/api/users", (req, res) => {
     });
 });
 
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(404)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    });
+});
+
 app.listen(5000, () => {
   console.log("API started on port 5000");
 });
